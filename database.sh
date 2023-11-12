9acce5befefb03714c058a9a8766fc323e6db8f8
Matt Riley <mriley@dimagi.com>
2023-11-12 16:50:03 -0500

    Database changes

--- database.sh
+++ database.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-# Ensure our user has docker permissions
-newgrp docker
-
 # Prompt to run containers
 echo "Please start up the containers if not already started"
-echo "In a separate terminal, please run 'scripts/docker up postgres couch redis elasticsearch5 zookeeper kafka minio'
+echo "In a separate terminal, please run 'scripts/docker up postgres couch redis elasticsearch5 zookeeper kafka minio'"
 echo "We've copied it to the clipboard to make it easy"
 echo "When the containers are ready, please type 'ready' below"
 echo -n "scripts/docker up postgres couch redis elasticsearch5 zookeeper kafka minio" | xclip -sel clip
@@ -18,7 +15,12 @@ fi
 read -r DB_NAME DB_USER DB_PASS < <(python -c 'import localsettings; db=localsettings.DATABASES["default"]; print("{} {} {}".format(db["NAME"], db["USER"], db["PASSWORD"]))')
 PGPASSWORD="$DB_PASS" psql -h localhost -p 5432 -U "$DB_USER" -c "CREATE DATABASE commcarehq;"
 
-#./manage.py sync_couch_views
-#./manage.py create_kafka_topics
-#env CCHQ_IS_FRESH_INSTALL=1 ./manage.py migrate --noinput
+read -r COUCH_USER COUCH_PASS < <(python -c 'import localsettings; db=localsettings.COUCH_DATABASES["default"]; print("{} {}".format(db["COUCH_USERNAME"], db["COUCH_PASSWORD"]))')
+
+curl -X PUT http://"$COUCH_USER":"$COUCH_PASS"@127.0.0.1:5984/_users
+curl -X PUT http://"$COUCH_USER":"$COUCH_PASS"@127.0.0.1:5984/_replicator
+
+./manage.py sync_couch_views
+./manage.py create_kafka_topics
+env CCHQ_IS_FRESH_INSTALL=1 ./manage.py migrate --noinput
 
--- docker.sh
+++ docker.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
+if [ -z "$SUDO_USER" ]; then
+    echo "Insufficient privileges. Please re-run this script as sudo"
+    exit 1
+fi
+
 # Add Docker's official GPG key:
 apt-get update
 apt-get install ca-certificates curl gnupg
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-chmod a+r /etc/apt/keyrings/docker.gpg
+if [ ! -f /etc/apt/keyrings/docker.gpg ]; then
+    install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    chmod a+r /etc/apt/keyrings/docker.gpg
+fi
 
 # Add the repository to Apt sources:
 echo \
@@ -14,10 +21,12 @@ echo \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 
-apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 groupadd docker
-usermod -aG docker $USER
+usermod -aG docker $SUDO_USER
+# Need to trigger a re-login to get the updated groups.
+# Both options below only apply to the current script. Best solution is a reboot
 #su - $USER
 #newgrp docker
 
@@ -25,5 +34,5 @@ systemctl is-active docker || systemctl start docker
 systemctl enable docker.service
 systemctl enable containerd.service
 
-chmod 0644 ./docker/files/elasticsearch*.yml
+chmod 0644 docker/files/elasticsearch*.yml
 
