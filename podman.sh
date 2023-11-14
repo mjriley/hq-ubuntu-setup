7695a5f96fe7d23a7334709e0eddbcc0bbc25867
Matt Riley <mriley@dimagi.com>
2023-11-13 23:40:26 -0500

    Better handle ubuntu mantic

--- podman.sh
+++ podman.sh
@@ -5,17 +5,28 @@ if [ -z "$SUDO_USER" ]; then
     exit 1
 fi
 
-if [ ! -f /etc/apt/sources.list.d/devel:kubic:libcontainers:unstable.list ]; then
-    mkdir -p /etc/apt/keyrings
-    curl -fsSL "https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/xUbuntu_$(lsb_release -rs)/Release.key" \
-      | gpg --dearmor \
-      | tee /etc/apt/keyrings/devel_kubic_libcontainers_unstable.gpg > /dev/null
-    echo \
-      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/devel_kubic_libcontainers_unstable.gpg]\
-        https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/xUbuntu_$(lsb_release -rs)/ /" \
-      | tee /etc/apt/sources.list.d/devel:kubic:libcontainers:unstable.list > /dev/null
-    apt-get update -qq
-fi
+UBUNTU_MAJOR_VERS=$(lsb_release -rs | cut -c-2)
+
+add_sources () {
+    if [[ "$UBUNTU_MAJOR_VERS" -ge "23" ]]; then
+        # appropriate podman versions are already available
+        return
+    fi
+
+    if [ ! -f /etc/apt/sources.list.d/devel:kubic:libcontainers:unstable.list ]; then
+        mkdir -p /etc/apt/keyrings
+        curl -fsSL "https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/xUbuntu_$(lsb_release -rs)/Release.key" \
+          | gpg --dearmor \
+          | tee /etc/apt/keyrings/devel_kubic_libcontainers_unstable.gpg > /dev/null
+        echo \
+          "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/devel_kubic_libcontainers_unstable.gpg]\
+            https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/xUbuntu_$(lsb_release -rs)/ /" \
+          | tee /etc/apt/sources.list.d/devel:kubic:libcontainers:unstable.list > /dev/null
+        apt-get update -qq
+    fi
+}
+
+add_sources
 
 # Clean previous podman installations, just in case
 apt remove -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
@@ -41,8 +52,11 @@ apt remove -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-
 # /usr/lib/systemd/user/podman.service
 
 apt install -y podman podman-docker
-# not ignoring recommended packages pulls a normal docker install -- we just need the docker-compose binary
-apt install --no-install-recommends -y docker-compose
+
+if [[ "$UBUNTU_MAJOR_VERS" -lt "23" ]]; then
+    # not ignoring recommended packages pulls a normal docker install -- we just need the docker-compose binary
+    apt install --no-install-recommends -y docker-compose
+fi
 
 # Install flatpak
 apt install -y flatpak
